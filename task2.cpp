ce74099a43749461e02badf1d8ae209ebe4a2110
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-26 00:18:26 +0500

    display message on screen if speed is greater than 100

---
+++ task2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int speed=0;
+	cout<<"enter car speed in Km/h:";
+	cin>> speed;
+	if(speed>100)
+	{
+		cout<<"you will be challaned";
+	}
+	else 
+	{
+		cout<<"nice you are a good driver:";
+	}
+
+}
